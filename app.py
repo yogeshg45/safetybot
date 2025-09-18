0b6b297647285905c8441364fa11c5ac24436506
yogeshg45 <151724436+yogeshg45@users.noreply.github.com>
2025-09-18 23:59:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -591,4 +591,6 @@ def dashboard():
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
